c9ea159e92ee8a6a6bc88b9c791e951aaf36ff5e
Anipnipnip <fauzhanif09@gmail.com>
2024-12-06 01:53:19 +0700

    updated

--- models/authModel.js
+++ models/authModel.js
@@ -2,14 +2,14 @@ import connection from "../config/db.js";
 
 const login = async(data) => {
     const db = await connection();
-    const [result] = await db.query('SELECT * FROM user WHERE email = ?', [data.email]);
+    const [result] = await db.query('SELECT * FROM users WHERE email = ?', [data.email]);
 
     return result
 }
 
 const signUp = async(data) => {
     const db = await connection();
-    await db.query(`INSERT INTO user(username, email, password) VALUES('${data.username}', '${data.email}', '${data.password}')`);
+    await db.query(`INSERT INTO users(username, email, password) VALUES('${data.username}', '${data.email}', '${data.password}')`);
 
     return true;
 }
